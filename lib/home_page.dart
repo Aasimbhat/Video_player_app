d2c8592538eece912b43d2502888ef3f1f700041
Kamran-7 <90784719+Aasimbhat@users.noreply.github.com>
2023-02-18 23:05:26 +0530

    Changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player_app/colors.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -9,6 +9,178 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Training",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: AppColor.homePageTitle,
+                      fontWeight: FontWeight.w700),
+                ),
+                Expanded(child: Container()),
+                Icon(
+                  Icons.arrow_back_ios,
+                  color: AppColor.homePageIcons,
+                  size: 20,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  color: AppColor.homePageIcons,
+                  size: 20,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColor.homePageIcons,
+                  size: 20,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Your Program",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: AppColor.homePageSubtitle,
+                      fontWeight: FontWeight.w700),
+                ),
+                Expanded(child: Container()),
+                Text(
+                  "Details",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: AppColor.homePageDetail,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Icon(
+                  Icons.arrow_forward,
+                  size: 20,
+                  color: AppColor.homePageIcons,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(80)),
+                  gradient: LinearGradient(colors: [
+                    AppColor.gradientFirst.withOpacity(0.8),
+                    AppColor.gradientSecond.withOpacity(0.9)
+                  ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(5, 10),
+                        blurRadius: 20,
+                        color: AppColor.gradientSecond.withOpacity(0.7))
+                  ]),
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, top: 25,right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Next Workout",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: AppColor.homePageContainerTextSmall),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Legs Toning",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: AppColor.homePageContainerTextSmall),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "and Glutes Workout",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: AppColor.homePageContainerTextSmall),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              size: 20,
+                              color: AppColor.homePageContainerTextSmall,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "60 min",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: AppColor.homePageContainerTextSmall),
+                            ),
+                          ],
+                        ),
+                        Expanded(child: Container()),
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: AppColor.gradientFirst,
+                                blurRadius: 10,
+                                offset: Offset(4,8)
+                              ),
+                              
+                            ],
+                            borderRadius: BorderRadius.circular(60)
+                          ),
+                          child: Icon(
+                            Icons.play_circle_fill,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
