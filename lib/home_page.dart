c3cfab64cfba6163a9e0e359024eedbcba6a4080
Kamran-7 <90784719+Aasimbhat@users.noreply.github.com>
2023-02-18 23:39:10 +0530

    changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -101,7 +101,7 @@ class _HomePageState extends State<HomePage> {
                         color: AppColor.gradientSecond.withOpacity(0.7))
                   ]),
               child: Container(
-                padding: const EdgeInsets.only(left: 20, top: 25,right: 20),
+                padding: const EdgeInsets.only(left: 20, top: 25, right: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -155,17 +155,12 @@ class _HomePageState extends State<HomePage> {
                         ),
                         Expanded(child: Container()),
                         Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
+                          decoration: BoxDecoration(boxShadow: [
+                            BoxShadow(
                                 color: AppColor.gradientFirst,
                                 blurRadius: 10,
-                                offset: Offset(4,8)
-                              ),
-                              
-                            ],
-                            borderRadius: BorderRadius.circular(60)
-                          ),
+                                offset: Offset(4, 8)),
+                          ], borderRadius: BorderRadius.circular(60)),
                           child: Icon(
                             Icons.play_circle_fill,
                             color: Colors.white,
@@ -176,6 +171,86 @@ class _HomePageState extends State<HomePage> {
                     )
                   ],
                 ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 140,
+              child: Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage("images/card.jpg"),
+                            fit: BoxFit.fill),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(8, 10),
+                              color: AppColor.gradientSecond.withOpacity(0.5)),
+                          BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(-1, -5),
+                              color: AppColor.gradientSecond.withOpacity(0.5))
+                        ]),
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.only(
+                      right: 200,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage("images/figure.png"),
+                      ),
+                    ),
+                  
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    margin: EdgeInsets.only(left: 150,top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You are doing great",
+                          style: TextStyle(
+                            fontSize:18,
+                            fontWeight:FontWeight.bold,
+                            color: AppColor.homePageDetail
+                          ),
+                        ),
+                        SizedBox(height: 10
+                        ),
+                        RichText(text: 
+                        TextSpan(
+                          text: "Keep it up \n",style: TextStyle(
+                            color: AppColor.homePagePlanColor,
+                            fontSize: 16
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "stick to your plan"
+                            )
+                          ]
+                        )
+                        ),
+                         
+
+                      ],
+                    ),
+                  )
+                ],
               ),
             )
           ],
